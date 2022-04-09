17e2761ee7dd9a307d772d8646cfe994a94b2076
Dylan Heathcote <dylanheathcote14@gmail.com>
2022-04-09 14:47:11 +0100

    navbar
    Signed-off-by: Dylan Heathcote <dylanheathcote14@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+// assets
+import CenLogo from './assets/CenWhite.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+         <div className="header-logo-container">
+           <img className="header-logo" src={CenLogo} />
+	 </div> 
+	 <div className="header-button-container">
+           <button className="header-button">Launch</button>
+	 </div>
       </header>
     </div>
   );
